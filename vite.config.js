b681f50225d8ffcdbad8602e55eba9fd0c60a83d
Samal <56509582+FransoArbela@users.noreply.github.com>
2025-09-05 22:52:36 +0200

    fix: Update paths in vite.config.js to use resolve for better compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: ".",
   build: {
     rollupOptions: {
       input: {
-        main: "./index.html",
-        login: "./login.html",
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "pages/auth/login.html"),
+        register: resolve(__dirname, "pages/auth/register.html"),
+        createListing: resolve(__dirname, "pages/listings/create-listing.html"),
+        singleListing: resolve(__dirname, "pages/listings/single-listing.html"),
+        profile: resolve(__dirname, "pages/profile/profile.html"),
       },
     },
   },
